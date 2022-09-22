b56009f9c0f38ef5c191dda35e1e14f0681b4b99
Michal Konecny <konecnym@prf.cuni.cz>
2022-09-22 21:46:51 +0200

    change condition to check if site was deleted + design changes

--- wp-content/themes/roottheme/index.php
+++ wp-content/themes/roottheme/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single posts
  *
@@ -16,40 +17,42 @@ get_header();
 <ul id="post-<?php the_ID(); ?>" class="subdomains-feed">
 
 	<?php
-		function get_screenshot($url, $id){
-			$filename = 'wp-content/uploads/screenshots/'.$id.'.png';
-			if (file_exists($filename)) return;
-			
-			$image = $id.'.png';
-			$accountKey = 'f863cc';
-			$ch = curl_init('http://api.screenshotmachine.com/?key='.$accountKey.'&dimension=320x240&format=png&url='.$url);
-			$fp = fopen('wp-content/uploads/screenshots/'.$image, 'wb');
-			curl_setopt($ch, CURLOPT_FILE, $fp);
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-			curl_exec($ch);
-			curl_close($ch);
-			fclose($fp);
+	function get_screenshot($url, $id)
+	{
+		$filename = 'wp-content/uploads/screenshots/' . $id . '.png';
+		if (file_exists($filename)) return;
+
+		$image = $id . '.png';
+		$accountKey = 'f863cc';
+		$ch = curl_init('http://api.screenshotmachine.com/?key=' . $accountKey . '&dimension=320x240&format=png&url=' . $url);
+		$fp = fopen('wp-content/uploads/screenshots/' . $image, 'wb');
+		curl_setopt($ch, CURLOPT_FILE, $fp);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_exec($ch);
+		curl_close($ch);
+		fclose($fp);
+	}
+
+	$subsites = get_sites(array('offset' => '1',));
+	foreach ($subsites as $subsite) {
+		$subsite_id = get_object_vars($subsite)["blog_id"];
+
+		if (get_blog_details($subsite_id)->deleted) {
+			continue;
 		}
 
-		$subsites = get_sites(array('offset' => '1',));
-		foreach( $subsites as $subsite ) {
-			$subsite_id = get_object_vars($subsite)["blog_id"];
-			if (!get_blog_details($subsite_id)->public) {
-				continue;
-			}
+		$subsite_name = get_blog_details($subsite_id)->blogname;
+		$subsite_url = get_site_url($subsite_id);
 
-			$subsite_name = get_blog_details($subsite_id)->blogname;
-			$subsite_url = get_site_url($subsite_id);
-		
-			get_screenshot($subsite_url, $subsite_id);
-			echo 	'<li class="card">
-						<div class="card__thumbnail" style="background-image: linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3)), url(wp-content/uploads/screenshots/' . $subsite_id . '.png);"></div>
-						<span class="card__title">' . $subsite_name .'</span>
-						<a href="' . $subsite_url . '" title="Přejít na stránku ' . $subsite_name .'" class="card__more"></a>
+		get_screenshot($subsite_url, $subsite_id);
+		echo 	'<li class="card ' . $is_public . '">
+						<div class="card__thumbnail" style="background-image: url(wp-content/uploads/screenshots/' . $subsite_id . '.png);"></div>
+						<span class="card__title">' . $subsite_name . '</span>
+						<a href="' . $subsite_url . '" title="Přejít na stránku ' . $subsite_name . '" class="card__more"></a>
 					 </li>';
-		}
+	}
 	?>
-			
+
 </ul>
 
 <?php
